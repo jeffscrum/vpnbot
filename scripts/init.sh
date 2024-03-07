b80f8f4ff52deb812d7a8c3a03120ad8e3c19337
Jeff Scrum <58530641+jeffscrum@users.noreply.github.com>
2024-03-07 12:24:57 +0300

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
+# Install prerequisites
 apt update
-apt install -y \
+apt install \
     ca-certificates \
     curl \
     gnupg \
@@ -10,16 +13,21 @@ apt install -y \
     iproute2 \
     xtables-addons-common \
     xtables-addons-dkms
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Install docker and docker-compose
+# mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') \
+#   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+# apt update
+# apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Install vpnbot
 git clone https://github.com/jeffscrum/vpnbot.git
 cd ./vpnbot
 echo "<?php
-
 \$c = ['key' => '$1'];" > ./app/config.php
-make u
+
+# Start vpnbot
+# make u
